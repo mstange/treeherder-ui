f352acd6040e1fd01c8c433e0e93cdb39a07aa34
Cameron Dawson <cdawson@mozilla.com>
2014-10-29 10:20:47 -0700

    bug 1072346 - pinboard no longer contained in bottom-panel
    the resizer directive seems to bleed values between being used in
    multiple places.  So I’m still working on how to get them independent.
    Otherwise, this is nearly working right.

--- webapp/app/vendor/resizer.js
+++ webapp/app/vendor/resizer.js
@@ -1,76 +1,55 @@
 /* From: http://stackoverflow.com/a/22253161/295132 (author: Mario Campa) */
-angular.module('mc.resizer', []).directive('resizer', function($document) {
-    return function($scope, $element, $attrs) {
-        $element.on('mousedown', function(event) {
-            event.preventDefault();
-            $document.on('mousemove', mousemove);
-            $document.on('mouseup', mouseup);
-        });
-
-        // the size last time this was called.  If it doesn't change,
-        // we should not modify the height/width value.  That can mess with
-        // scrollbars, etc.
-        var lastOffset;
-
-        function mousemove(event) {
-            if ($attrs.resizer == 'vertical') {
-                // Handle vertical resizer
-                var x = event.pageX;
-                if ($attrs.resizerMax && x > $attrs.resizerMax) {
-                    x = parseInt($attrs.resizerMax);
-                }
-                $element.css({
-                    left: x + 'px'
-                });
-                $($attrs.resizerLeft).css({
-                    width: x + 'px'
-                });
-                $($attrs.resizerRight).css({
-                    left: (x + parseInt($attrs.resizerWidth)) + 'px'
-                });
-            } else {
-                // Handle horizontal resizer
-                var container, y, relY;
-                if ($attrs.resizerContainer) {
-                    // a resizer that is in a container other than the
-                    // whole window
-                    container = $($attrs.resizerContainer);
-                    relY = event.pageY - container.offset().top;
+angular.module('mc.resizer', []).directive('resizer',
+    function($document) {
+    return {
+        link: function($scope, $element, $attrs) {
+            $element.on('mousedown', function (event) {
+                event.preventDefault();
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+            function mousemove(event) {
+                if ($attrs.resizer == 'vertical') {
+                    // Handle vertical resizer
+                    var x = event.pageX;
+                    if ($attrs.resizerMax && x > $attrs.resizerMax) {
+                        x = parseInt($attrs.resizerMax);
+                    }
+                    $element.css({
+                                     left: x + 'px'
+                                 });
+                    $($attrs.resizerLeft).css({
+                                                  width: x + 'px'
+                                              });
+                    $($attrs.resizerRight).css({
+                                                   left: (x + parseInt($attrs.resizerWidth)) + 'px'
+                                               });
                 } else {
-                    // this is a resizer for two elements of the whole window.
-                    container = $(window);
-                    relY = event.pageY;
-                }
-                y = container.innerHeight() - relY;
-
-                var top = $($attrs.resizerTop),
-                    bottom = $($attrs.resizerBottom),
-                    oldEl = $element.css("bottom"),
-                    oldTop = top.css("bottom"),
-                    oldBottom = bottom.css("height");
-
-                // set the elements to their new values
-                $element.css({bottom: y + 'px'});
-                top.css({bottom: (y + parseInt($attrs.resizerHeight)) + 'px'});
-                bottom.css({height: y + 'px'});
-
-                var newOffset = $($element).offset().top;
-                // if, after setting to the new values, the offset of the
-                // resizer doesn't actually change, then we want to revert
-                // the height.  Having the height set to something it isn't
-                // able to be will mess with scrollbars.
-                if (newOffset !== lastOffset) {
-                    lastOffset = newOffset;
-                } else {
-                    $element.css({bottom: oldEl});
-                    top.css({bottom: oldTop});
-                    bottom.css({height: oldBottom});
+                    // Handle horizontal resizer
+                    var adjust = 0;
+                    if ($attrs.resizerBottomPad) {
+                        //                    adjust = $($attrs.resizerBottomPad).offset().top;
+                        adjust = $($attrs.resizerBottomPad).height();
+                        //                    console.log("adjust", adjust, $($attrs.resizerBottomPad).offset());
+                    }
+                    var y = window.innerHeight - event.pageY;
+                    console.log("y value", window.innerHeight, event.pageY, adjust, y);
+                    $element.css({
+                                     bottom: y + 'px'
+                                 });
+                    $($attrs.resizerTop).css({
+                                                 bottom: (y + parseInt($attrs.resizerHeight)) + 'px'
+                                             });
+                    $($attrs.resizerBottom).css({
+                                                    height: y + 'px'
+                                                });
                 }
             }
-        }
-        function mouseup() {
-            $document.unbind('mousemove', mousemove);
-            $document.unbind('mouseup', mouseup);
+
+            function mouseup() {
+                $document.unbind('mousemove', mousemove);
+                $document.unbind('mouseup', mouseup);
+            }
         }
     };
 });
